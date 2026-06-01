801db331dd2e308cd67f29c8754f9886d6be7b26
Máximo <messinamaximo@gmail.com>
2026-06-01 09:59:38 -0300

    agrego rutas de post alumno y buscador

--- routes/alumno.routes.js
+++ routes/alumno.routes.js
@@ -1,12 +1,16 @@
 const { Router } = require('express')
 const {
   getAlumnoAll,
-  getAlumnoById
+  getAlumnoById,
+  createAlumno,
+  getAlumnoBySearch
 } = require('../controllers/alumno.controller')
 
 const rutas = Router()
 
 rutas.get('/', getAlumnoAll)
 rutas.get('/:legajo', getAlumnoById)
+rutas.post('/', createAlumno)
+rutas.get('/search', getAlumnoBySearch)
 
 module.exports = rutas
